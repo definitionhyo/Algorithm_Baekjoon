33d0a82a0fb0326d4e84cdc2a9fd6232984b6c17
definitionhyo <definitionhyo@gmail.com>
2024-01-24 22:48:49 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 버퍼리더를 통해 받음
+		String id = br.readLine(); // readLine을통해 받아온 문자열을 id에 저장
+		System.out.println( id + "??!" ); // id 뒤에 ??! 추가
+		br.close();
+		
+	}
+
+}
